7a2c2d0d355185575a7157579e5762f8e9fd5262
louismarie <12646487+louismarie@users.noreply.github.com>
2022-02-19 14:20:30 +0100

    fix: create user allow any

--- back/api/views.py
+++ back/api/views.py
@@ -2,13 +2,16 @@ from django.http import HttpResponse
 from rest_framework import viewsets, permissions
 from django.contrib.auth.models import User, Group
 from api.serializers import UserSerializer, GroupSerializer
+from rest_framework.decorators import action
+from rest_framework.permissions import AllowAny, IsAdminUser
+
 
 class UserViewSet(viewsets.ModelViewSet):
     queryset = User.objects.all().order_by('-date_joined')
     serializer_class = UserSerializer
-    permission_classes = [permissions.IsAuthenticated, permissions.IsAdminUser]
 
-class GroupViewSet(viewsets.ModelViewSet):
-    queryset = Group.objects.all()
-    serializer_class = GroupSerializer
-    permission_classes = [permissions.IsAuthenticated, permissions.IsAdminUser]
+    def get_permissions(self):
+        if self.action == 'create':
+            return (AllowAny(),)
+        return (IsAdminUser(),)
+
