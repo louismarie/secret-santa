7a5619dbde844b3bb62e5b6be6ee20854791738b
louismarie <12646487+louismarie@users.noreply.github.com>
2022-02-19 10:59:27 +0100

    feat: add auth with rest framework

--- back/api/urls.py
+++ back/api/urls.py
@@ -1,7 +1,27 @@
-from django.urls import path
-
+from django.urls import include,path
+from django.contrib.auth.models import User
+from rest_framework import serializers, viewsets, routers
 from . import views
 
+# Serializers define the API representation.
+class UserSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = User
+        fields = ['username', 'email']
+
+# ViewSets define the view behavior.
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+
+# Routers provide a way of automatically determining the URL conf.
+router = routers.DefaultRouter()
+router.register(r'users', UserViewSet)
+
+
 urlpatterns = [
+    path('', include(router.urls)),
     path('alive', views.index, name='index'),
+    path('auth', include('rest_framework.urls', namespace='rest_framework')),
 ]
