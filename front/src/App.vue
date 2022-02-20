eab32e12bd9a369ee247f12b9d8c2c9088605bc5
louismarie <12646487+louismarie@users.noreply.github.com>
2022-02-20 08:39:03 +0100

    feat: add register screen

--- front/src/App.vue
+++ front/src/App.vue
@@ -1,19 +1,19 @@
 <template>
   <v-app>
     <v-main>
-      <HelloWorld/>
+      <Register/>
     </v-main>
   </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Register from './components/Register.vue'
 
 export default {
   name: 'App',
 
   components: {
-    HelloWorld,
+    Register,
   },
 
   data: () => ({
---
+++ front/src/components/Register.vue
@@ -0,0 +1,67 @@
+<template>
+  <v-container>
+    <v-row class="text-center">
+      <v-col class="mb-4">
+        <h1 class="display-2 font-weight-bold mb-3">
+          <div>Welcome to Secret Santa</div>
+        </h1>
+      </v-col>
+
+      <v-col
+        class="mb-5"
+        cols="12"
+      >
+        <h2 class="headline font-weight-bold mb-5">
+          Register
+        </h2>
+      </v-col>
+    </v-row>
+    <v-row justify="center">
+      <v-col cols="6">
+        <v-text-field
+            label="Username"
+        ></v-text-field>
+      </v-col>
+    </v-row>
+    <v-row justify="center">
+     <v-col cols="6">
+        <v-text-field
+            label="Email"
+        ></v-text-field>
+      </v-col>
+    </v-row>
+    <v-row justify="center">
+      <v-col cols="6">
+        <v-text-field
+            label="Password"
+        ></v-text-field>
+      </v-col>
+    </v-row>
+    <v-row justify="center">
+      <v-col>
+        <v-row class="mb-5" justify="center">
+          <v-btn variant="outlined"
+                 color="primary">
+            Register now
+          </v-btn>
+        </v-row>
+        <v-row justify="center">
+          <a>Already have account ?</a>
+        </v-row>
+      </v-col>
+
+
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+
+export default {
+  name: 'HelloWorld',
+
+  data: () => ({
+
+  }),
+}
+</script>
